926bc3e2aba167f85dd0bc4703bcc40cc6a6e5f3
zhliw <zhangliwen574@outlook.com>
2026-05-07 21:21:53 +0800

    Add files via upload

---
+++ src/main/java/com/fund/demo/controller/AdminController.java
@@ -0,0 +1,8 @@
+package com.fund.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+}
---
+++ src/main/java/com/fund/demo/controller/StatisticController.java
@@ -0,0 +1,8 @@
+package com.fund.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/statistic")
+public class StatisticController {
+}
