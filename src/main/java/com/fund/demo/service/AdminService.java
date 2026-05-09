f3152313571cd6956535779f704ba5e3d7762888
zhliw <zhangliwen574@outlook.com>
2026-05-09 12:40:39 +0800

    Update AdminService.java

--- src/main/java/com/fund/demo/service/AdminService.java
+++ src/main/java/com/fund/demo/service/AdminService.java
@@ -1,4 +1,16 @@
-package com.fund.demo.service;
+package com.example.demo.service;
 
+import com.example.demo.entity.Admin;
+import com.example.demo.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public Admin login(String adminName, String adminPassword) {
+        return adminRepository.findByAdminNameAndAdminPassword(adminName, adminPassword);
+    }
 }
