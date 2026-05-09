d996c616d1b455c037a59c59fb466620a48173a5
zhliw <zhangliwen574@outlook.com>
2026-05-09 11:51:29 +0800

    Update User.java

--- src/main/java/com/fund/demo/entity/User.java
+++ src/main/java/com/fund/demo/entity/User.java
@@ -1,4 +1,25 @@
-package com.fund.demo.entity;
+package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Double monthlyBudget;
 }
