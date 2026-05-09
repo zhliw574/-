98ac4c9a60dd8fd6f319292787600951a713ef34
zhliw <zhangliwen574@outlook.com>
2026-05-09 11:49:45 +0800

    Update Admin.java

--- src/main/java/com/fund/demo/entity/Admin.java
+++ src/main/java/com/fund/demo/entity/Admin.java
@@ -1,4 +1,22 @@
-package com.fund.demo.entity;
+package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "admins")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+
+    @Column(unique = true, nullable = false)
+    private String adminName;
+
+    @Column(nullable = false)
+    private String adminPassword;
+
+    @Column(nullable = false)
+    private String email;
 }
