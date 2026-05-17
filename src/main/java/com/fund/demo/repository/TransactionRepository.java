fbb59d29c21147e9c07f3e0c020a94f686d35561
zhliw <zhangliwen574@outlook.com>
2026-05-17 22:04:15 +0800

    Update TransactionRepository.java

--- src/main/java/com/fund/demo/repository/TransactionRepository.java
+++ src/main/java/com/fund/demo/repository/TransactionRepository.java
@@ -2,12 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByUserIdAndType(Long userId, String type);
-    List<Transaction> findByUserId(Long userId);
+    List<Transaction> findByUserUserId(Long userId);
     List<Transaction> findByType(String type);
+    List<Transaction> findByUserUserIdAndType(Long userId, String type);
 }
