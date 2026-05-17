058c871d18f73d9639034ac2f330cbbb77575aa2
zhliw <zhangliwen574@outlook.com>
2026-05-17 22:03:25 +0800

    Update User.java

--- src/main/java/com/fund/demo/entity/User.java
+++ src/main/java/com/fund/demo/entity/User.java
@@ -17,9 +17,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false)
     private Double monthlyBudget;
 }
